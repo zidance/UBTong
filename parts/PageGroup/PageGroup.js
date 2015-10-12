2f0a4c543fc367b3cb8de28d62a251e5268ef4de
dingdong-io <fromio@163.com>
2015-10-12 18:50:54 +0800

    页面组合 v0.5
    在jQuery下,可以实现带js的页面组合.
    目前zepto只能组合页面,js效果多数会失效

--- parts/PageGroup/PageGroup.js
+++ parts/PageGroup/PageGroup.js
@@ -1,20 +1,15 @@
-//var $$ = jQuery;  //jQuery
- var $$ = $;  //zepto
+var $$ = jQuery;  //jQuery 需交出$
+// var $$ = window.Zepto;  //zepto
 
-//zepto与jQuery的$冲突问题
+//zepto与jQuery的$$冲突问题
 //其它js不生效问题
 
-
-  //模块集合的初始化,将需要的模块路径按 ['url1','url2',...] 的格式写入 本地时只能是相对 只接受斜杠,不接受反斜杠是因引号内需转义
-  
 
 $$(function(){
   //默认的初始化 其中部分可配置,属全局pageGroup={}
   var defaultPageGroup={ //可升级,
     title:'组合页面',//组合页面的标题
-//    //是否引用注释--注释容错,<!--...<link..><script..>-->
 //    //公用的css和js路径
-//      //css，js，写入位置的开关
     hasError:false,
     errors:{
       page:[],
@@ -25,11 +20,11 @@ $$(function(){
     
   };
 
-  // $$("#group").click(function(){
+
       
   var aPage=[];
   var iPageNum = 0;
-    $("head").prepend("<title>"+(pageGroup.title||defaultPageGroup.title)) 
+    $$("head").prepend("<title>"+(pageGroup.title||defaultPageGroup.title)) 
   var main = function(){//主循环函数,不采用for是避免异步ajax造成变量的混乱
     
 
@@ -45,7 +40,7 @@ $$(function(){
       type:"GET",
 //      async:true,
       error:function(){//可升级,思路:一本地测试,二路径错误时存变量,并在for结束后统一回馈
-        $("body").prepend("<div class='errorInfo'><p style='color:red'>第<span>"+(iPageNum+1)+"</span>个路径有误,请检查.</p><p>如果是在本机测试(没有架设服务器),请参照 <a target='_blank' href='http://blog.sina.com.cn/s/blog_a76aa1590101eams.html' >这里</a></p>")
+        $$("body").prepend("<div class='errorInfo'><p style='color:red'>第<span>"+(iPageNum+1)+"</span>个路径有误,请检查.</p><p>如果是在本机测试(没有架设服务器),请参照 <a target='_blank' href='http://blog.sina.com.cn/s/blog_a76aa1590101eams.html' >这里</a></p>")
         
 //        pageGroup.hasError=true;
 //        pageGroup.errors.page[pageGroup.errors.pageCount] = iPageNum;
@@ -80,9 +75,9 @@ $$(function(){
           .replace(/([\r\n]\s*)+/mg,'\n')
         
         var wrapTemp = "pageGroupTemp"+iPageNum
-        $("body").append('<div class="'+wrapTemp+'">')
+        $$("body").append('<div class="'+wrapTemp+'">')
         wrapTemp = '.'+wrapTemp
-        $(wrapTemp)
+        $$(wrapTemp)
           .append(oP.head)
           .append(oP.body)
           .append('<div class="pageGroupTempChild">')
@@ -91,41 +86,34 @@ $$(function(){
         //方案1，body改为div　问题：可能对一些css设置和js获取dom树产生影响，如body>h1
         //方案2，div生成后，再获取其属性赋予到body并脱掉div壳，id,class,style属性，id的处理，再升级就是遍历属性了
         //方案3，对body直接查找到">"，将字段传递给新body，也要处理body多id
-//        alert($(".pageGroupBody").html())
+//        alert($$(".pageGroupBody").html())
         
         
         
         //url判断和处理 
         oP.path = oP.url.replace(/(.*\/).*/,'$1')
-//        if(oP.url.substring(0,2)===".."){
-//          oP.urlType = "relative"
-//        }else{//绝对路径,可能是 /xxx,也可以http://
-//          oP.urlType = "absolute"
-//        }
+        if(oP.url.substring(0,2)===".."){
+          oP.urlType = "relative"
+        }else{//绝对路径,可能是 /xxx,或http://
+          oP.urlType = "absolute"
+        }
         
    //资源的路径重写 对DOM属性操作 缺乏对相对的判断
-        $(wrapTemp).find('*').each(function(i){
-          if($(this).attr('href')){//存在href属性
-            $(this).attr('href',oP.path+$(this).attr('href'))
+        $$(wrapTemp).find('*').each(function(i){
+          if($$(this).attr('href')){//存在href属性
+            $$(this).attr('href',oP.path+$$(this).attr('href'))
           }
-          if($(this).attr('src')){//存在 src
-            $(this).attr('src',oP.path+$(this).attr('src'))
+          if($$(this).attr('src')){//存在 src
+            if((/:\/\//m.test($$(this).attr('src')))||($$(this).attr('src').substring(0,1)==='/')||($$(this).attr('src').substring(0,1)==='\\')){//资源绝对路径
+               }else{//资源相对路径
+                 $$(this).attr('src',oP.path+$$(this).attr('src'))
+               }
+            
           }
         })
         
-        
-        //直接对res字符串操作 只需处理相对 因重度依赖res/ 放弃 
-//        oP.tempUrl = res.match(/(\.\.\/)+/)[0]//匹配当前第一个
-//        oP.tempPath = function(){
-//          
-//        }
-//        for(var i=1;i<=oP.tempUrl.match(/(\.\.\/)+?/g).length;i++){
-//        }
-//       
-        
-        
         //删除额外的节点
-//     $('.pageGroupTempChild').unwrap().remove()
+//     $$('.pageGroupTempChild').unwrap().remove()
      
       iPageNum++
       main()
@@ -139,13 +127,11 @@ $$(function(){
 
   }//多个模块的for循环结束
       
-      
-      
-        
+       
 }//main
-      
-  main()    
-//alert(aPage[1].url)
+  main()   
+  
+
   //对引用模块的html文件进行基本验证,head/body标签有且只有1 body的属性
   function isHtml(){
 
@@ -156,7 +142,7 @@ $$(function(){
 //    setTimeout(function(){
 ////    if(pageGroup.hasError){
 //
-//      $("body").prepend("<div class='errorInfo'><p style='color:red'>第<span></span>个路径有误,请检查.</p><p>如果是在本机测试(没有架设服务器),请参照 <a target='_blank' href='http://blog.sina.com.cn/s/blog_a76aa1590101eams.html' >这里</a></p>")//中贡文
+//      $$("body").prepend("<div class='errorInfo'><p style='color:red'>第<span></span>个路径有误,请检查.</p><p>如果是在本机测试(没有架设服务器),请参照 <a target='_blank' href='http://blog.sina.com.cn/s/blog_a76aa1590101eams.html' >这里</a></p>")//中贡文
 //      var errorPage=''
 ////      alert(typeof pageGroup.errors.page)
 //      for(var i in pageGroup.errors.page){
@@ -164,19 +150,19 @@ $$(function(){
 //        alert(pageGroup.errors.page[i])
 //      }
 //     
-//     $('.errorInfo').find("span").text(errorPage)
+//     $$('.errorInfo').find("span").text(errorPage)
 //      
 ////    }
 //      },2000)
   }
 
 
- // })
+
   
   
 })
 
-//alert(pageGroup)
+
 
 /* 
 /([\r\n]\s*)+/mg连空格
