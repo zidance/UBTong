eb57066ca802ec84cea555acc0e84b6a35e8c40b
dingdong-io <fromio@163.com>
2015-10-16 18:04:23 +0800

    放大在ios微信上存在兼容问题,暂存

--- details/layout1/pic/pic2/res/js/banner.js
+++ details/layout1/pic/pic2/res/js/banner.js
@@ -1,18 +1,33 @@
 // JavaScript Document
-var active=0,
-	as=document.getElementById('pagenavi').getElementsByTagName('a');
-	
-for(var i=0;i<as.length;i++){
-	(function(){
-		var j=i;
-		as[i].onclick=function(){
-			t2.slide(j);
-			return false;
-		}
-	})();
+var active = 0;
+
+//按图片的数量增加导航点
+var a = '<a href="###"></a>'
+$('#pagenavi').empty()
+for (nu = $('.slider').find('img').length - 1; nu >= 0; nu--) {
+    $('#pagenavi').append(a)
 }
-var t2=new TouchSlider({id:'sliderlist', speed:600, timeout:6000, before:function(index){
-		as[active].className='';
-		active=index;
-		as[active].className='active';
-	}});
+$('#pagenavi').find('a').first().addClass('active')
+
+
+as = document.getElementById('pagenavi').getElementsByTagName('a');
+
+for (var i = 0; i < as.length; i++) {
+    (function () {
+        var j = i;
+        as[i].onclick = function () {
+            t2.slide(j);
+            return false;
+        }
+    })();
+}
+var t2 = new TouchSlider({
+    id: 'sliderlist',
+    speed: 600,
+    timeout: 6000,
+    before: function (index) {
+        as[active].className = '';
+        active = index;
+        as[active].className = 'active';
+    }
+});
