eb82c99e50a4ddf9d8ec51a48999cf7acbe9082a
hrpIng <512615967@qq.com>
2015-10-22 09:16:20 +0800

    1px

--- common/js/1pxProblem.js
+++ common/js/1pxProblem.js
@@ -2,177 +2,99 @@ $(function(){
 	var _all=$('*');
 	for(var i=0; i<_all.length;i++){
 		/*四周都有边框*/
+		var aPosition = ['left', 'right', 'top', 'bottom'];
+		var varColor,varRadiu,oParents,varwidth;
+		varColor=_all.eq(i).css('border-color');
+		varRadiu=_all.eq(i).css('border-radius');
+		varwidth=_all.eq(i).css('border-width');
+		
+		oParents = _all.eq(i).parents();
+		var sStyleChain='';
+		
+		/*获取父级元素*/
+		function getParents(){
+			for(var nTemp = oParents.length-1;nTemp>=0;nTemp--){
+				var sClassName='';
+				if(oParents.get(nTemp).className){
+					sClassName=	oParents.get(nTemp).className;
+					sStyleChain += (oParents.get(nTemp).localName+'.'+sClassName+' ');	
+				}else{
+					sStyleChain += (oParents.get(nTemp).localName+' ');	
+				}
+			   
+		   }
+			
+		}
+		/*加上各自的Class*/
+		function AddClass(_borderClass){
+		  if(varRadiu=='0px'){
+			  _all.eq(i).append("<style>"+sStyleChain+"."+_borderClass+"::before{ border-color:" +varColor+ ";border-width:"+varwidth+";border-radius:0px}</style>");      
+		   }else{
+			  _all.eq(i).append("<style>"+sStyleChain+"."+_borderClass+"::before{ border-color:" +varColor+ ";border-width:"+varwidth+";border-radius:" +varRadiu+ "}</style>");         
+		   } 
+		   if(_all.eq(i).css('position')=='fixed'||_all.eq(i).css('position')=='absolute'){
+			}else{
+				_all.eq(i).css('position','relative');
+			};
+	   }
+	   
+	   function AddClass1(_borderClass){
+		  if(varRadiu=='0px'){
+				  $('.border-1px').append("<style>"+sStyleChain+".border-top-bottom::after{ border-color:" +varColor+ ";border-radius:0px}"+sStyleChain+".border-top-bottom::before{ border-color:" +varColor+ "};</style>");      
+			   }else{
+				  $('.border-1px').append("<style>"+sStyleChain+".border-top-bottom::after{ border-color:" +varColor+ ";border-radius:" +varRadiu+ "};"+sStyleChain+".border-top-bottom::before{ border-color:" +varColor+ ";</style>");         
+			   }
+		   
+		   if(_all.eq(i).css('position')=='fixed'||_all.eq(i).css('position')=='absolute'){
+			}else{
+				_all.eq(i).css('position','relative');
+			};
+	   }
+	   
+	   
+	   /*判断边的情况*/
 
-		if(parseInt(_all.eq(i).css('border-top-width'))==1&&parseInt(_all.eq(i).css('border-bottom-width'))==1&&parseInt(_all.eq(i).css('border-left-width'))==1&&parseInt(_all.eq(i).css('border-right-width'))==1){
-			 var varColor=_all.eq(i).css('border-color');
-			 var varRadiu=_all.eq(i).css('border-radius');
+		if(varwidth=='1px'){
 			_all.eq(i).css('border','none');
 			_all.eq(i).addClass('border-1px');
-			
-			var oParents = _all.eq(i).parents();
-			   var sStyleChain = '';
-			   for(var nTemp = oParents.length-1;nTemp>=0;nTemp--){
-				   sStyleChain += (oParents.get(nTemp).localName+' ')
-			   }
-			   
-			   if(varRadiu=='0px'){
-				  $('.border-1px').append("<style>"+sStyleChain+".border-1px::after{ border-color:" +varColor+ ";border-radius:0px}</style>");      
-			   }else{
-				  $('.border-1px').append("<style>"+sStyleChain+".border-1px::after{ border-color:" +varColor+ ";border-radius:" +varRadiu+ "}</style>");         
-			   }
-	
-			
-
-			if(_all.eq(i).css('position')=='fixed'||_all.eq(i).css('position')=='absolute'){
-			}else{
-				$('.border-1px').css('position','relative');
-			};
-
+			getParents();
+			AddClass('border-1px');   
 			/*上下都有边框*/
-		}else if(parseInt(_all.eq(i).css('border-top-width'))==1&&parseInt(_all.eq(i).css('border-bottom-width'))==1){
-			var varColor=_all.eq(i).css('border-color');
-			var varRadiu=_all.eq(i).css('border-radius');
+		}else if(varwidth=='1px 0px'||varwidth=='1px 0px 1px 0px'){
 			_all.eq(i).css('border','none');
 			_all.eq(i).addClass('border-top-bottom');	
-			
-			   var oParents = _all.eq(i).parents();
-			   var sStyleChain = '';
-			   for(var nTemp = oParents.length-1;nTemp>=0;nTemp--){
-				   sStyleChain += (oParents.get(nTemp).localName+' ')
-			   }
-			   
-			   if(varRadiu=='0px'){
-				  $('.border-top-bottom').append("<style>"+sStyleChain+".border-top-bottom::after{ border-color:" +varColor+ ";border-radius:0px}</style>");      
-			   }else{
-				  $('.border-top-bottom').append("<style>"+sStyleChain+".border-top-bottom::after{ border-color:" +varColor+ ";border-radius:" +varRadiu+ "}</style>");         
-			   }
-
-
-			if(_all.eq(i).css('position')=='fixed'||_all.eq(i).css('position')=='absolute'){
-			}else{
-				$('.border-top-bottom').css('position','relative');
-			};
-			
+			getParents();
+			AddClass1('border-top-bottom'); 
 			/*左右都有边框*/
-		}else if(parseInt(_all.eq(i).css('border-left-width'))==1&&parseInt(_all.eq(i).css('border-right-width'))==1){
-			var varColor=_all.eq(i).css('border-color');
-			var varRadiu=_all.eq(i).css('border-radius');
+		}else if(varwidth=='0px 1px'||varwidth=='0px 1px 0px 1px'){
 			_all.eq(i).css('border','none');
 			_all.eq(i).addClass('border-left-right');	
-			
-			var oParents = _all.eq(i).parents();
-			   var sStyleChain = '';
-			   for(var nTemp = oParents.length-1;nTemp>=0;nTemp--){
-				   sStyleChain += (oParents.get(nTemp).localName+' ')
-			   }
-	
-			if(varRadiu=='0px'){
-				  $('.border-left-right').append("<style>"+sStyleChain+".border-left-right::after{ border-color:" +varColor+ ";border-radius:0px}</style>");      
-			   }else{
-				  $('.border-left-right').append("<style>"+sStyleChain+".border-left-right::after{ border-color:" +varColor+ ";border-radius:" +varRadiu+ "}</style>");         
-			   }
-
-			if(_all.eq(i).css('position')=='fixed'||_all.eq(i).css('position')=='absolute'){
-			}else{
-				$('.border-left-right').css('position','relative');
-			};
-			
+			getParents();
+			AddClass('border-left-right'); 
 			/*上面有边框*/
 		}else if(parseInt(_all.eq(i).css('border-top-width'))==1){
-			var varColor=_all.eq(i).css('border-color');
-			var varRadiu=_all.eq(i).css('border-radius');
 			_all.eq(i).css('border','none');
 			_all.eq(i).addClass('border-top');	
-			
-			var oParents = _all.eq(i).parents();
-			   var sStyleChain = '';
-			   for(var nTemp = oParents.length-1;nTemp>=0;nTemp--){
-				   sStyleChain += (oParents.get(nTemp).localName+' ')
-			   }
-	
-			if(varRadiu=='0px'){
-				  $('.border-top').append("<style>"+sStyleChain+".border-top::after{ border-color:" +varColor+ ";border-radius:0px}</style>");      
-			   }else{
-				  $('.border-top').append("<style>"+sStyleChain+".border-top::after{ border-color:" +varColor+ ";border-radius:" +varRadiu+ "}</style>");         
-			   }
-			
-			if(_all.eq(i).css('position')=='fixed'||_all.eq(i).css('position')=='absolute'){
-			}else{
-				$('.border-top').css('position','relative');
-			};
-		}
-		
+			getParents();
+			AddClass('border-top');
 			/*下面有边框*/
-		else if(parseInt(_all.eq(i).css('border-bottom-width'))==1){
-			var varColor=_all.eq(i).css('border-color');
-			var varRadiu=_all.eq(i).css('border-radius');
+		}else if(parseInt(_all.eq(i).css('border-bottom-width'))==1){
 			_all.eq(i).css('border','none');
 			_all.eq(i).addClass('border-bottom');
-				
-			var oParents = _all.eq(i).parents();
-			   var sStyleChain = '';
-			   for(var nTemp = oParents.length-1;nTemp>=0;nTemp--){
-				   sStyleChain += (oParents.get(nTemp).localName+' ')
-			   }
-	
-			if(varRadiu=='0px'){
-				  $('.border-bottom').append("<style>"+sStyleChain+".border-top::after{ border-color:" +varColor+ ";border-radius:0px}</style>");      
-			   }else{
-				  $('.border-bottom').append("<style>"+sStyleChain+".border-top::after{ border-color:" +varColor+ ";border-radius:" +varRadiu+ "}</style>");         
-			   }
-			if(_all.eq(i).css('position')=='fixed'||_all.eq(i).css('position')=='absolute'){
-			}else{
-				$('.border-bottom').css('position','relative');
-			};
-		}
-		
-		  /*左面有边框*/
-		else if(parseInt(_all.eq(i).css('border-left-width'))==1){
-			var varColor=_all.eq(i).css('border-color');
-			var varRadiu=_all.eq(i).css('border-radius');
+			getParents();
+			AddClass('border-bottom');
+			 /*左面有边框*/
+		}else if(parseInt(_all.eq(i).css('border-left-width'))==1){
 			_all.eq(i).css('border','none');
 			_all.eq(i).addClass('border-left');	
-			
-			var oParents = _all.eq(i).parents();
-			   var sStyleChain = '';
-			   for(var nTemp = oParents.length-1;nTemp>=0;nTemp--){
-				   sStyleChain += (oParents.get(nTemp).localName+' ')
-			   }
-	
-			if(varRadiu=='0px'){
-				  $('.border-left').append("<style>"+sStyleChain+".border-left::after{ border-color:" +varColor+ ";border-radius:0px}</style>");      
-			   }else{
-				  $('.border-left').append("<style>"+sStyleChain+".border-left::after{ border-color:" +varColor+ ";border-radius:" +varRadiu+ "}</style>");         
-			   }
-			if(_all.eq(i).css('position')=='fixed'||_all.eq(i).css('position')=='absolute'){
-			}else{
-				$('.border-left').css('position','relative');
-			};
-		}
-		
-		
-		  /*右面有边框*/
-		else if(parseInt(_all.eq(i).css('border-right-width'))==1){
-			var varColor=_all.eq(i).css('border-color');
-			var varRadiu=_all.eq(i).css('border-radius');
+			getParents();
+			AddClass('border-left');
+			/*右面有边框*/
+		}else if(parseInt(_all.eq(i).css('border-right-width'))==1){
 			_all.eq(i).css('border','none');
 			_all.eq(i).addClass('border-right');	
-			
-			var oParents = _all.eq(i).parents();
-			   var sStyleChain = '';
-			   for(var nTemp = oParents.length-1;nTemp>=0;nTemp--){
-				   sStyleChain += (oParents.get(nTemp).localName+' ')
-			   }
-	
-			if(varRadiu=='0px'){
-				  $('.border-right').append("<style>"+sStyleChain+".border-right::after{ border-color:" +varColor+ ";border-radius:0px}</style>");      
-			   }else{
-				  $('.border-right').append("<style>"+sStyleChain+".border-right::after{ border-color:" +varColor+ ";border-radius:" +varRadiu+ "}</style>");         
-			   }
-			if(_all.eq(i).css('position')=='fixed'||_all.eq(i).css('position')=='absolute'){
-			}else{
-				$('.border-right').css('position','relative');
-			};
+			getParents();
+			AddClass('border-right');
 		}
 		
 		
