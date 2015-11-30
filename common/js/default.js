1427c3480780ecaae13ae64deaf9932d36cffc99
dingdong-io <fromio@163.com>
2015-11-30 14:26:28 +0800

    [Fixbug-1pxborder] 修改核心文件
    Common.css 只留下.border类
    Default.js 允许任意边框组合,边框的粗细/样式/颜色都为自动获取

--- common/js/default.js
+++ common/js/default.js
@@ -25,114 +25,101 @@ function changeLeftNav(){
  * 模块说明：
  * @1pxProblem 移动端1px脚本
  */
-$(function(){
-	var _all=$('*');
-	
-	for(var i=0; i<_all.length;i++){	
-		/*四周都有边框*/
-		var varColor,varRadiu,oParents,varwidth,varstyle;
-		varColor=_all.eq(i).css('border-color');
-		varRadiu=parseInt(_all.eq(i).css('border-radius'))+5;
-		varRadiu=varRadiu+'px';
-		varwidth=_all.eq(i).css('border-width');
-		varstyle=_all.eq(i).css('border-style');
-		var _this=_all.eq(i);
-		oParents = _all.eq(i).parents();
-		var sStyleChain=''; 
-		
-		
-		/*获取父级元素*/
-		function getParents(){
-			sStyleChain=''; 
-			for(var nTemp = oParents.length-1;nTemp>=0;nTemp--){
-				var sClassName='';
-				if(oParents.get(nTemp).className){
-					sClassName=	oParents.get(nTemp).className;
-					if(sClassName.indexOf(" ")> -1){
-						sClassName=sClassName.substring( 0, sClassName.indexOf(' '));	
-					}
-					sStyleChain += (oParents.get(nTemp).localName+'.'+sClassName+' ');	
-				}else{
-					sStyleChain += (oParents.get(nTemp).localName+' ');	
-				}
-			}
-		}
+$(function () {
+
+  var _all = $('*'),
+    aPosition = ['top', 'right', 'bottom', 'left']
+
+  $('head').append('<style id="fix-1px" rel="stylesheet" type="text/css">');
+
+  /*获取祖先元素链,加强优先级*/
+    var getParents = function(obj) {
+      var oParents = obj.parents()
+      sStyleChain = '';
+      for (var nTemp = oParents.length - 1; nTemp >= 0; nTemp--) {
+        var sClassName = '';
+        if (oParents.get(nTemp).className) {
+          sClassName = oParents.get(nTemp).className;
+          if (sClassName.indexOf(" ") > -1) {
+            sClassName = sClassName.replace(/ /g, '.');
+          }
+          sStyleChain += (oParents.get(nTemp).localName + '.' + sClassName + ' ');
+        } else {
+          sStyleChain += (oParents.get(nTemp).localName + ' ');
+        }
+      }
+    }
+
+    /*判断定位*/
+    var getPosition = function(obj) {
+      if (obj.css('position') != 'fixed' && obj.css('position') != 'absolute') {
+        obj.css('position', 'relative');
+      }
+    }
 
 
-		/*判断定位*/
-		function getPosition(){    
-			if(_this.css('position')=='fixed'||_this.css('position')=='absolute'){
-			}else{
-				_this.css('position','relative');
-			};
-		}
-		
-		
-		
-		
-		/*加上各自的Class*/
+  for (var i = _all.length - 1; i >= 0; i--) {
 
-		function AddTop(_borderClass){
-			_this.addClass(_borderClass);
-			_this.append("<style>"+sStyleChain+"."+_borderClass+"::before{ background:"+varColor+"}</style>"); 
-			getPosition();    
-		}
-		
-		function AddBottom(_borderClass1){
-			_this.addClass(_borderClass1);   
-			_this.append("<style>"+sStyleChain+"."+_borderClass1+"::after{ background:"+varColor+"}</style>");         
-			getPosition();        
-		}
-		
-		function AddLeft(_borderClass2){
-			_this.addClass(_borderClass2);
-			_this.append("<style>"+sStyleChain+"."+_borderClass2+"::before{ background:"+varColor+"}</style>");         
-			getPosition();        
-		}
-		function AddRight(_borderClass){
-			_this.addClass(_borderClass);
-			_this.append("<style>"+sStyleChain+"."+_borderClass+"::after{ background:"+varColor+"}</style>");         
-			getPosition();        
-		}
-		function AddBorder(_borderClass){
-			_this.addClass(_borderClass);
-			if(varRadiu=='0px'){
-				_this.append("<style>"+sStyleChain+"."+_borderClass+"::after{ border-color:"+varColor+";border-radius:0px;border-style:"+varstyle+"}</style>");      
-			}else{
-				_this.append("<style>"+sStyleChain+"."+_borderClass+"::after{ border-color:"+varColor+";border-radius:"+varRadiu+";border-style:"+varstyle+"}</style>");         
-			} 
-			getPosition();        
-		}
-		
-		/*判断边的情况*/
-				
-		if(varwidth=='1px'){
-			_this.css('border','none');
-			getParents();
-			AddBorder('trblBor');
-		}else{
-			if(_this.css('border-top-width')=='1px'){
-				_this.css('border-top','none');
-				getParents();
-				AddTop('tBor');
-			}
-			if(_this.css('border-bottom-width')=='1px'){
-				_this.css('border-bottom','none');
-				getParents();
-				AddBottom('bBor');
-			}
-			if(_this.css('border-left-width')=='1px'){
-				_this.css('border-left','none');
-				getParents();
-				AddLeft('lBor');
-			}
-			
-			if(_this.css('border-right-width')=='1px'){
-				_this.css('border-right','none');
-				getParents();
-				AddRight('rBor');
-			}
-			
-		}	
-	}	
+    var oThis = _all.eq(i);
+    var sStyleText = '';
+    var sStyleChain = '';
+
+
+    /*样式收集与写属性 参数表是否四方向*/
+    var writeStyle = function (isAllSides) {
+      var iCount = 1;
+      //元素border的方向循环
+      for (var k = 3; k >= 0; k--) {
+        if (oThis.css('border-' + aPosition[k] + '-width') != '0px') {//无定义时也是返0px
+
+          var sBorder = 'border-' + aPosition[k];
+          var sBorderAttr = oThis.css(sBorder);
+
+
+          //祖先元素链,只有在第一次发现某方向上有border时执行
+          if (iCount == 1) {
+            iCount = 0
+            getPosition(oThis)
+            getParents(oThis)
+          }
+
+          //样式收集
+          sStyleText += sBorder + ':' + sBorderAttr + ';'
+        }
+
+        //跳出四方向循环前,写入样式
+        if (k == 0 && iCount != 1) {
+          if (isAllSides) { //四边时多处理圆角情况
+
+            var sRadius = oThis.css('border-radius');
+
+            /*px翻倍才能保证缩放后与原容器稳合,百分比的不变*/
+            if (!(/%/.test(sRadius)) && window.devicePixelRatio >= 1.5) {
+              sRadius = sRadius.replace(/\d/, Number(sRadius.match(/\d/)) * 2)
+            }
+            if (sRadius != '0px') {
+              sStyleText += 'border-radius:' + sRadius + ';'
+            }
+          }
+
+          /*写入元素计数器,以免li:last-child 之类的影响到同级元素*/
+            $('#fix-1px').prepend('\n' + sStyleChain + '.fix-border' + i + '::after{' + sStyleText + '}')
+          oThis.addClass('fix-border' + i + ' border');
+          oThis.css('border', 'none')
+        }
+      }
+    }
+
+    /*四面有border*/
+    if (oThis.css('border-width') == '1px') {
+      writeStyle(true)
+    }
+    //非四面都有border,包括无border
+    else {
+      writeStyle(false)
+    }
+
+
+  }
+
 })
