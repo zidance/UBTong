900a38b40dd69e9875119106162133d821266fc2
dingdong-io <fromio@163.com>
2015-12-01 11:51:48 +0800

    default.js完善了对border-radius的动态判断
    接受各边不一样的border-radius
    调试对象页面:seller/layout1/popularize/detail/switch/switch1/切换.html

--- common/js/default.js
+++ common/js/default.js
@@ -63,63 +63,58 @@ $(function () {
     var oThis = _all.eq(i);
     var sStyleText = '';
     var sStyleChain = '';
+    var iCount = 1,sRadius='';
 
+    /*样式收集与写属性*/
 
-    /*样式收集与写属性 参数表是否四方向*/
-    var writeStyle = function (isAllSides) {
-      var iCount = 1;
       //元素border的方向循环
       for (var k = 3; k >= 0; k--) {
-        if (oThis.css('border-' + aPosition[k] + '-width') != '0px') {//无定义时也是返0px
 
+        //border-width遍历
+        if (oThis.css('border-' + aPosition[k] + '-width') != '0px') {//无定义时也是返0px
           var sBorder = 'border-' + aPosition[k];
           var sBorderAttr = oThis.css(sBorder);
-
-
           //祖先元素链,只有在第一次发现某方向上有border时执行
           if (iCount == 1) {
             iCount = 0
             getPosition(oThis)
             getParents(oThis)
           }
-
           //样式收集
           sStyleText += sBorder + ':' + sBorderAttr + ';'
         }
 
-        //跳出四方向循环前,写入样式
-        if (k == 0 && iCount != 1) {
-          if (isAllSides) { //四边时多处理圆角情况
-
-            var sRadius = oThis.css('border-radius');
-
+        //border-radius遍历
+        switch(k){
+          case 3:sRadius = 'border-top-left-radius';break;
+          case 2:sRadius = 'border-top-right-radius';break;
+          case 1:sRadius = 'border-bottom-left-radius';break;
+          default:sRadius = 'border-bottom-right-radius';break;
+        }
+        if (oThis.css(sRadius) != '0px') {//无定义时也是返0px
+          var sRadiusAttr = oThis.css(sRadius);
             /*px翻倍才能保证缩放后与原容器稳合,百分比的不变*/
-            if (!(/%/.test(sRadius)) && window.devicePixelRatio >= 1.5) {
-              sRadius = sRadius.replace(/\d/, Number(sRadius.match(/\d/)) * 2)
+            if (!(/%/.test(sRadiusAttr)) && window.devicePixelRatio >= 1.5) {
+              sRadiusAttr = sRadiusAttr.replace(/\d*/, Number(sRadiusAttr.match(/\d*/)) * 2)
             }
-            if (sRadius != '0px') {
-              sStyleText += 'border-radius:' + sRadius + ';'
-            }
-          }
+              sStyleText += sRadius + ':' + sRadiusAttr + ';'
 
+          if (iCount == 1) {
+            iCount = 0
+            getPosition(oThis)
+            getParents(oThis)
+          }
+        }
+
+        //跳出四方向循环前,写入样式
+        if (k == 0 && iCount == 0) {
+console.log('\n' + sStyleChain + '.fix-border' + i + '::after{' + sStyleText + '}')
           /*写入元素计数器,以免li:last-child 之类的影响到同级元素*/
             $('#fix-1px').prepend('\n' + sStyleChain + '.fix-border' + i + '::after{' + sStyleText + '}')
           oThis.addClass('bd fix-border' + i);
           oThis.css('border', 'none')
         }
       }
-    }
-
-    /*四面有border*/
-    if (oThis.css('border-width') == '1px') {
-      writeStyle(true)
-    }
-    //非四面都有border,包括无border
-    else {
-      writeStyle(false)
-    }
-
-
   }
 
 })
