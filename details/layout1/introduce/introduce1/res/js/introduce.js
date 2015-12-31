1e9f41eca85979f201a39de6665b18442fadc0b8
dingdong-io <fromio@163.com>
2015-12-31 17:38:25 +0800

    [Add-Wudu] 详情页（商品介绍部分），评价管理，订单评价三个页面，采用了填充饱满图像的形式。附升级公用图像脚本（套页面时发现因图像加载快慢问题脚本对部分图不生效，新脚本已复）

--- details/layout1/introduce/introduce1/res/js/introduce.js
+++ details/layout1/introduce/introduce1/res/js/introduce.js
@@ -1,4 +1,6 @@
+
 $(function () {
+
     var _obj = $('#DetailsIntroduce');
     var _btn = _obj.find('nav a');
     var _item = _obj.find('.introduce-con .introduce-item');
@@ -19,30 +21,32 @@ $(function () {
     _btn.first().trigger('click')
         //第一次点评价时,执行图片尺寸脚本
     var oBtnAppraise = _obj.find('.btn-appraise')
-    oBtnAppraise.one('click', function () {
-        imgZoomControl();
+    oBtnAppraise.one('click', function (e) {
+      //图片尺寸
+      img('.appraise-img','span')
+        e.preventDefault();
     })
+
+    //评价 加载更多交互
+    var appraise = _obj.find('.appraise')
+    appraise.find('.a-intgbook').on('tap click',function(){
+        appraise.find('ul').append(appraise.find('li').first().clone())
+        //ajax后的图片再触发一次调整;
+        //img('.appraise-img','span')
+    })
+
 })
 
 
-/*图片图片尺寸脚本,大图缩小,小图不放大,定义尺寸,居中靠css*/
-function imgZoomControl() {
-    var oImgWraps = $('.details-introduce').find('.appraise-img').find('span'),
-        nWidth = oImgWraps.eq(0).width();
-    oImgWraps.find('img').css({
-        'max-width': nWidth,
-        'max-height': nWidth
-    })
-
     /*全屏展示大图开与关*/
-    oImgWraps.on('tap click', function () {
+    $('.details-introduce').on('click tap','.appraise-img span', function () {
         $('.stn-mask-layer').show()
     })
     $('.imgzoom_pack').on('click tap', function () {
         $('.imgzoom_pack').hide();
         $('.stn-mask-layer').hide()
     })
-}
+
 
 /*全屏可缩放大图初始化对象 ,依赖于第三方touchzoom.js*/
 document.addEventListener("DOMContentLoaded", function (event) {
